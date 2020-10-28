1c5d7dd5d2671b95a82d17438a3fbd4d921928a1
Созонов Евгений Александрович <sozonovea@main.sgu.ru>
2020-10-28 12:42:27 +0400

    1

--- МНК к Гаусу/Mnk.py
+++ МНК к Гаусу/Mnk.py
@@ -35,13 +35,36 @@ def gaus(a,b):
 
 
     X = [0 for d in range(L)]
-    
+
     for i in range(len(b) - 1, -1, -1):
         X[i] = (b[i] - sum(x * j for x, j in zip(X[(i + 1):], a[i][(i + 1):])))/a[i][i]
     print("\n".join("X{0} =\t{1:10.2f}".format(i + 1, x) for i, x in
     enumerate(X)))
 
     return X
+X = []
+Y = []
+LenY=len(Y)
 
+for i in np.arange(-3,3,0.01):
+    X.append(i)
+    Y.append(i**3)
 
+Aij = np.zeros((LenY,LenY))
+Bi= np.zeroz(LenY)
+
+for i in range(LenY):
+    for j in range(LenY):
+        Aij[i][j] = ( X[i]**3 ) * ( X[i]**3 )
+
+for i in range(LenY):
+    Bi[i] = ( X[i]**3 ) * Y[i]
+
+C = gaus(Aij,Bi)
+
+plt.subplot(1,2,1)
+plt.plot(Y)
+plt.subplot(1,2,2)
+plt.plot(X)
+plt.show()
 
