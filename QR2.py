ee5286a84e53c3d147fee4592ecf49a78b96dc0d
Созонов Евгений Александрович <sozonovea@main.sgu.ru>
2020-12-01 11:31:50 +0400

    1

--- QR2.py
+++ QR2.py
@@ -8,36 +8,42 @@ A = np.loadtxt('1.txt')
 def QR(A):
     #копирование и транспонирование матрицы
     At = A.transpose()
-    Qt = deepcopy(At)
-    #размеры матрицы
-    Qshape0 = np.shape(Qt)[0]
-    Qshape1 = np.shape(Qt)[1]
-    print(np.shape(Qt))
-    # объявление матрциы r
-    r = np.zeros((Qshape1,Qshape1))
-    #нормировка первой строки
-    norm = np.dot(Qt[0],At[0])
-    for i in range(Qshape1):
-        Qt[0][i] = Qt[0][i]/math.sqrt(norm)
+    shape = np.shape(At)
+    Qt = np.zeros((shape[0],shape[1]))
+    r = np.zeros((shape[0],shape[0]))
 
-    for k in range(Qshape0):
-        for j in range(Qshape1):
-            if j<k:
-                Vkj = np.dot(Qt[j],At[k])
-                Qt[k]= Qt[k]-Vkj*Qt[j]
-                norm = np.dot(Qt[0],At[0])
-                for i in range(Qshape1):
-                    Qt[k][i]=Qt[0][i]/math.sqrt(norm)
+    for i in range(shape[0]):
+        for j in range(shape[1]):
+            Qt[i][j]=At[i][j]
+    
+    norm = np.dot(Qt[0],Qt[0])
+    for i in range(shape[1]):
+        Qt[0][i] = Qt[0][i]/math.sqrt(norm)
+    
+    for k in range(1,shape[0]):
+        for j in range(shape[0]):
+            if j < k:
+                Vkj = np.dot(Qt[j],Qt[k])
+                Qt[k]=Qt[k]-Vkj*Qt[j]
+                norm2 = dot(Qt[k],Qt[k])
+                for i in range(shape[1]):
+                    Qt[k][i]=Qt[k][i]/math.sqrt(norm2)
                 
-    for i in range (Qshape1):
-        for j in range(Qshape1):
+
+                
+    for i in range (shape[0]):
+        for j in range(shape[0]):
             if j >=i:
-                r[i][j]= np.dot(At[i],Qt[j])
+                r[i][j]= np.dot(At[j],Qt[i])
             else:
                 r[i][j]=0
     print(Qt)
     print('\n',r)
     return 0
 
+
 QR(A)
+Qb,rD = np.linalg.qr(A)
+print('\n',Qb.T)
+print('\n',rD)
 
